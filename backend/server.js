fc384cf5b30cfffa78ff839b38074ef7b653accc
judejochimson judechihan <judechihanachchu@gmail.com>
2025-12-17 21:45:59 +0530

    feat: Initialize Express server with database connection and product table creation.

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,8 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import productRoutes from "./routes/productRoutes.js";
+import { sql } from "../config/db.js";
+
 
 const app = express();
 
@@ -18,6 +20,25 @@ app.use(morgan("dev"));
 
 app.use("/api/products", productRoutes);
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+async function initDB() {
+  try {
+    await sql`
+        CREATE TABLE IF NOT EXISTS products (
+            id SERIAL PRIMARY KEY,
+            name VARCHAR(255) NOT NULL,
+            image VARCHAR(255) NOT NULL,
+            price DECIMAL(10,2) NOT NULL,
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        )
+    `;
+    console.log("Database initialized successfully");
+  } catch (error) {
+    console.error("Error initializing database:", error);
+  }
+}
+
+initDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  })
 });

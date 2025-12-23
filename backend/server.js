ad20203d22de29283ec9c4e5a4c0cc23296ce7ca
JUDECHIHAN J. <judechihan727@gmail.com>
2025-12-23 20:01:00 +0530

    feat: Initialize Express server with Arcjet protection and product database setup.

--- backend/server.js
+++ backend/server.js
@@ -9,8 +9,6 @@ import productRoutes from "./routes/productRoutes.js";
 
 import { sql } from "./config/db.js";
 
-
-
 const app = express();
 
 const PORT = process.env.PORT || 5001;
@@ -19,6 +17,36 @@ app.use(express.json());
 app.use(cors());
 app.use(helmet());
 app.use(morgan("dev"));
+
+app.use(async (req, res, next) => {
+  try {
+    const decision = await aj.protect(req, {
+      requested: 1,
+    });
+    if (decision.isDenied()) {
+      if (decision.reason.isRateLimit()) {
+        res.status(429).json({ error: "Too many requests" });
+      } else if (decision.reason.isBot()) {
+        res.status(403).json({ error: "Bot access denied" });
+      } else {
+        res.status(403).json({ error: "Access denied" });
+      }
+      return;
+    }
+    if (
+      decision.results.some(
+        (result) => result.reason.isBot() && result.reason.isSpoofed()
+      )
+    ) {
+      res.status(403).json({ error: "spoof bot  detected" });
+      return;
+    }
+    next();
+  } catch (error) {
+    console.log("Arcjet error!", error);
+    next(error);
+  }
+});
 
 app.use("/api/products", productRoutes);
 
@@ -42,5 +70,5 @@ async function initDB() {
 initDB().then(() => {
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
-  })
+  });
 });

d4314d71a926d96c3eedc27ddd898073c36df5ed
JUDECHIHAN J. <judechihan727@gmail.com>
2025-12-26 22:23:32 +0530

    feat: Implement initial full-stack application with product CRUD, Arcjet security, and frontend state management.

--- backend/server.js
+++ backend/server.js
@@ -4,20 +4,24 @@ import morgan from "morgan";
 import cors from "cors";
 import dotenv from "dotenv";
 import aj from "./lib/arcjet.js";
-dotenv.config();
+import path from "path";
+import { fileURLToPath } from "url";
 
+dotenv.config();
 
 import productRoutes from "./routes/productRoutes.js";
 
 import { sql } from "./config/db.js";
 
 const app = express();
-
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cors());
-app.use(helmet());
+app.use(helmet({
+  contentSecurityPolicy: false,
+}));
 app.use(morgan("dev"));
 
 app.use(async (req, res, next) => {
@@ -51,6 +55,13 @@ app.use(async (req, res, next) => {
 });
 
 app.use("/api/products", productRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+  app.get(/.*/, (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
 
 async function initDB() {
   try {
